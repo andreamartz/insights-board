0c261abafcb2c846807c52b5763690016704f58f
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2026-04-21 14:21:54 -0500

    Update relative imports to be path alias imports for consistency

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import DashboardShell from '../../components/DashboardShell';
+import DashboardShell from '@/components/DashboardShell';
 
 // return type is JSX.Element, but the modern preference is to omit this annotation unless the tsconfig or lint rules require it
 const DashboardPage = () => {
